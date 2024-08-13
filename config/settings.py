4f3dd720f86e89bd3f321df2eff7daad944ebb9d
anatoly <anatolyches@gmail.com>
2024-08-13 17:52:43 +0300

    change envs + uncomment host/port connection

--- config/settings.py
+++ config/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'NAME': os.getenv('NAME_DB'),
         'USER': os.getenv('USER_DB'),
         'PASSWORD': os.getenv('PASSWORD_DB'),
-        # 'HOST': os.getenv('HOST_DB'),
-        # 'PORT': os.getenv('PORT_DB'),
+        'HOST': os.getenv('HOST_DB'),
+        'PORT': os.getenv('PORT_DB'),
     }
 }
 
